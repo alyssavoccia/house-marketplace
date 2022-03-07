b20a8b71f51f16e0c3a2dd6e7a3219d4125e606a
Alyssa Voccia <avoccia@hotmail.com>
2022-03-07 15:49:18 -0500

    Added the ability to register users

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { db } from '../firebase.config';
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
+import { namedQuery } from 'firebase/firestore';
 
 function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
@@ -21,6 +24,26 @@ function SignUp() {
     }));
   };
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+
+      const user = userCredential.user;
+
+      updateProfile(auth.currentUser, {
+        displayName: namedQuery
+      });
+
+      navigate('/');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div className="pageContainer">
@@ -28,7 +51,7 @@ function SignUp() {
           <p className="pageHeader">Welcome Back!</p>
         </header>
 
-        <form>
+        <form onSubmit={onSubmit}>
           <input type="text" className='nameInput' placeholder='Name' id='name' value={name} onChange={onChange} />
           <input type="email" className='emailInput' placeholder='Email' id='email' value={email} onChange={onChange} />
 
